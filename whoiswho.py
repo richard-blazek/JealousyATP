42a65f70b99be9bdbdd959532fbddf6f7c653457
RichardBlazek <blazek.risa@gmail.com>
2023-05-17 19:25:31 +0200

    Little fix

--- whoiswho.py
+++ whoiswho.py
@@ -1,11 +1,11 @@
 import td
 import json
+import sys
 
 if not td.authorize():
     sys.exit('Not authorized')
 
 contacts = {user['id']: user['last_name'] + ' ' + user['first_name'] for user in td.get_users(td.get_contacts())}
 
-with open('names.txt', mode = 'w') as out:
-    print(json.dumps(contacts), file = out)
-
+with open('names.txt', mode = 'wb') as out:
+    out.write(json.dumps(contacts, ensure_ascii=False).encode())
